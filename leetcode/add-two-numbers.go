2a2d801932785d331dd510ee03ea2f68a1bc0774
hathai25 <phamhuyhathai@gmail.com>
2023-09-20 22:36:52 +0700

    update

---
+++ leetcode/add-two-numbers.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	result := &ListNode{}
+	tmp := result
+	for l1 != nil || l2 != nil {
+		if l1 != nil {
+			tmp.Val += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			tmp.Val += l2.Val
+			l2 = l2.Next
+		}
+		if tmp.Val > 9 {
+			tmp.Val -= 10
+			tmp.Next = &ListNode{Val: 1}
+		} else if l1 != nil || l2 != nil {
+			tmp.Next = &ListNode{}
+		}
+		tmp = tmp.Next
+	}
+	return result
+}
+
+func main() {
+
+	//new list node
+	l1 := new(ListNode)
+	l2 := new(ListNode)
+
+	//add values to list node
+	l1.Val = 2
+	l1.Next = new(ListNode)
+	l1.Next.Val = 4
+	l1.Next.Next = new(ListNode)
+	l1.Next.Next.Val = 3
+
+	l2.Val = 5
+	l2.Next = new(ListNode)
+	l2.Next.Val = 6
+	l2.Next.Next = new(ListNode)
+	l2.Next.Next.Val = 4
+
+	l3 := addTwoNumbers(l1, l2)
+
+	for l3 != nil {
+		fmt.Print(l3.Val)
+		l3 = l3.Next
+	}
+
+}
